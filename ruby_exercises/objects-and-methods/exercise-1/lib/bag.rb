3adba886bbb5f0396eb3899e513633755da07ecf
Logan Sauer <ldsauer2@gmail.com>
2024-12-12 20:04:19 -0700

    "finished the hashes_spec"

--- ruby_exercises/objects-and-methods/exercise-1/lib/bag.rb
+++ ruby_exercises/objects-and-methods/exercise-1/lib/bag.rb
@@ -1,9 +1,11 @@
 
 class Bag
-    attr_reader :empty
+    attr_reader :empty, :count, :candies
 
     def initialize
         @empty = true
+        @count = 0
+        @candies = []
     end
 
     def empty?
