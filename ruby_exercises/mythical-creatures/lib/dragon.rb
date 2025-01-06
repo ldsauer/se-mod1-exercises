b3aa25d38fe56676bc5a747bd56f67359531f1ff
Logan Sauer <ldsauer2@gmail.com>
2025-01-06 09:20:44 -0700

    commiting everthing I have done so far

---
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -0,0 +1,12 @@
+class Dragon
+    attr_reader :name, :color, :rider
+
+    def initialize(name, color, rider)
+        @name = name
+        @color = color
+        @rider = rider
+    end
+
+    def hungry? 
+end
+
